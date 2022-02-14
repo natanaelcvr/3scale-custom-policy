bc11a5835bf4199e37c8e6ebe60bba372ca58660
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2022-02-14 17:22:38 -0300

    last functional steps

--- local-apicast.sh
+++ local-apicast.sh
@@ -1,17 +1,19 @@
-TOKEN=3096735a55cd468878745e424c323c3afdee180cce15e58fea5d7928f6e0605c
-3SCALE_URL=3scale-admin.apps.cluster-a1a6.a1a6.sandbox1817.opentlc.com
+TOKEN=f6d72b42f17dd38113af394404524febbe0903c4205740f8d0b5d3760151312b
+TSCALE_URL=3scale-admin.apps.cluster-b851.b851.sandbox243.opentlc.com
 IMAGE_ADDRESS=registry.access.redhat.com/3scale-amp20/apicast-gateway:1.0
 APICAST_CONFIGURATION_CACHE=0
 THREESCALE_DEPLOYMENT_ENV=staging
 APICAST_CONFIGURATION_LOADER=lazy
 APICAST_LOG_LEVEL=debug
-THREESCALE_CONFIG_FILE=config.json
+THREESCALE_CONFIG_FILE=apicast-config-api-sandbox-1.json
 
-docker run --rm -p 8080:8080 \
-    -v $(pwd)/policy:/opt/app/policies/jwt-token-introspection-policy/1.0 \
-    -e THREESCALE_CONFIG_FILE=config.json
+docker run --rm -it -p 8080:8080 \
+    -v "$(pwd)/policy:/opt/app-root/policies/jwt-token-introspection-policy/1.0" \
+    -v "$(pwd):/opt/app-root/custom" \
+    -e THREESCALE_CONFIG_FILE=custom/apicast-config-api-sandbox-1.json \
     -e APICAST_CONFIGURATION_CACHE=$APICAST_CONFIGURATION_CACHE \
     -e THREESCALE_DEPLOYMENT_ENV=$THREESCALE_DEPLOYMENT_ENV \
     -e APICAST_CONFIGURATION_LOADER=$APICAST_CONFIGURATION_LOADER \
     -e APICAST_LOG_LEVEL=$APICAST_LOG_LEVEL \
-    -e THREESCALE_PORTAL_ENDPOINT=https://$TOKEN@$3SCALE_URL $IMAGE_ADDRESS
+    -e THREESCALE_PORTAL_ENDPOINT="https://$TOKEN@$TSCALE_URL" \
+    $IMAGE_ADDRESS
--- redeploy.sh
+++ redeploy.sh
@@ -1,8 +1,8 @@
 oc delete secret jwe-token-introspection
 oc create secret generic jwe-token-introspection \
-    --from-file=./apicast-policy.json \
-    --from-file=./init.lua \
-    --from-file=./jwe-token-introspection.lua \
-    --from-file=./tokens_cache.lua
+    --from-file=./policy/apicast-policy.json \
+    --from-file=./policy/init.lua \
+    --from-file=./policy/jwe-token-introspection.lua \
+    --from-file=./policy/tokens_cache.lua
 oc get pods | grep my-gateway | awk '{ print $1 }' | xargs oc delete pod
 oc get pods -w | grep my-gateway 
